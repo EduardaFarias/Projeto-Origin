9d80e631aa77ab640f55143579e35b424482f8b3
EduardaFarias <maria.eduarda.farias@ccc.ufcg.edu.br>
2023-04-05 09:32:21 -0300

    fix:comentários decódigo

--- Missão Arduino/missao-arduino.cpp
+++ Missão Arduino/missao-arduino.cpp
@@ -9,17 +9,20 @@ void setup()
 
 void loop()
 {
-  digitalWrite(13, HIGH); // O pino 13 possui sinal alto, apresen-
-  //tando 5v eletricamente
-  delay(1000); // passa 1 segundo ligado
-  digitalWrite(13, LOW); // Sina
-  delay(1000);
+  digitalWrite(13, HIGH); // Liga o LED
+  delay(1000); // LED permanece 1 segundo ligado
+  digitalWrite(13, LOW); // LED desligado
+  
+  delay(1000); // LED 1 segundo desligado
+  
+  digitalWrite(13, HIGH); 
+  delay(2000); // LED 2 segundos ligado
+  digitalWrite(13, LOW); 
+  
+  delay(1000); 
+  
   digitalWrite(13, HIGH);
-  delay(2000); 
-  digitalWrite(13, LOW);
-  delay(1000);
-  digitalWrite(13, HIGH);
-  delay(5000); 
+  delay(5000); // LED 5 segundos ligado
   digitalWrite(13, LOW);
   delay(1000);
 }
